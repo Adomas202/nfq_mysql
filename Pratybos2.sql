1462b3d9366a99a37a3ee1d74718934ad7dd5695
Poleninas <adomas202@gmail.com>
2018-10-19 00:52:54 +0300

    Structure just changed bit

--- Pratybos2.sql
+++ Pratybos2.sql
@@ -1,7 +1,5 @@
 1.	INSERT INTO authors (name) VALUES ("Tomas Gilys"), ("Andrius Tapinas");
 
-2.	INSERT INTO books (authorId, title, year) VALUES (8, "NFQ akademija kelias", 2018),
-												 (9, "Need For Speed", 2016),
-												 (10, "Duomenu bazes ir daugiau", 2012);
+2.	INSERT INTO books (authorId, title, year) VALUES (8, "NFQ akademija kelias"), (8, "Need For Speed", 2016), (9, "Duomenu bazes ir daugiau", 2012);
 
-3.	UPDATE books SET authorId = 6 WHERE bookId = 4;
+3.	UPDATE books SET authorId = 8 WHERE authorId IN NULL LIMIT 1;
